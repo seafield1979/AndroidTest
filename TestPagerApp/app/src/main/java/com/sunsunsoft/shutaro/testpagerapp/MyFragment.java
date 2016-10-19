4e1e8cfd8db45fa3076f208bcf332287568241d5
shusuke unno <seafield1979@gmail.com>
2016-10-19 21:57:45 +0900

    モダンなアプリのテンプレート
    ViewPager
    Drawer

---
+++ TestPagerApp/app/src/main/java/com/sunsunsoft/shutaro/testpagerapp/MyFragment.java
@@ -0,0 +1,36 @@
+package com.sunsunsoft.shutaro.testpagerapp;
+
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class MyFragment extends Fragment {
+    private final static String BACKGROUND_COLOR = "background_color";
+
+    public static MyFragment newInstance(@ColorRes int IdRes) {
+        MyFragment frag = new MyFragment();
+        Bundle b = new Bundle();
+        b.putInt(BACKGROUND_COLOR, IdRes);
+        frag.setArguments(b);
+        return frag;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, null);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.fragment_main_linearlayout);
+        linearLayout.setBackgroundResource(getArguments().getInt(BACKGROUND_COLOR));
+
+        return view;
+    }
+}
