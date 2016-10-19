f2ff2a2f3fff170472f2e95d7284463a448f3dc1
shusuke unno <seafield1979@gmail.com>
2016-10-19 15:48:18 +0900

    ドラッグ＆ドロップサンプルの修正
    リファクタリング

--- TestDragDrop/app/src/main/java/com/sunsunsoft/shutaro/testdragdrop/MainActivity.java
+++ TestDragDrop/app/src/main/java/com/sunsunsoft/shutaro/testdragdrop/MainActivity.java
@@ -20,38 +20,44 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
     private int insertPos;
     private int startPos;
 
-    private ImageView[] imageViews = new ImageView[4];
+    private int[] view_ids = new int[]{
+            R.id.imageView,
+            R.id.imageView2,
+            R.id.imageView3,
+            R.id.imageView4
+    };
+    private ImageView[] imageViews = new ImageView[view_ids.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 並び替えるViewImagesが入ったLinearLayout
         mContainer = (ViewGroup)findViewById(R.id.container1);
+        mContainer.setOnDragListener(this);
 
-        imageViews[0] = (ImageView)findViewById(R.id.imageView);
-        imageViews[0].setBackgroundColor(Color.rgb(255,0,0));
-        imageViews[1] = (ImageView)findViewById(R.id.imageView2);
-        imageViews[1].setBackgroundColor(Color.rgb(0,255,0));
-        imageViews[2] = (ImageView)findViewById(R.id.imageView3);
-        imageViews[2].setBackgroundColor(Color.rgb(255,0,255));
-        imageViews[3] = (ImageView)findViewById(R.id.imageView4);
-        imageViews[3].setBackgroundColor(Color.rgb(255,255,0));
-
+        // 並び替える項目(ImageView)
         for (int i=0; i<imageViews.length; i++) {
+            imageViews[i] = (ImageView)findViewById(view_ids[i]);
             imageViews[i].setOnLongClickListener(this);
         }
+        imageViews[0].setBackgroundColor(Color.rgb(255,0,0));
+        imageViews[1].setBackgroundColor(Color.rgb(0,255,0));
+        imageViews[2].setBackgroundColor(Color.rgb(255,0,255));
+        imageViews[3].setBackgroundColor(Color.rgb(255,255,0));
 
+        // 挿入位置を見せるためのView
         mInsertPosView = new ImageView(this);
         ViewGroup.LayoutParams layout = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
-        layout.height = 144;
+        layout.height = 144;//imageViews[0].getHeight();
         mInsertPosView.setLayoutParams(layout);
 
-        mContainer.setOnDragListener(this);
     }
 
+    // リストのアイテムを長押し、でドラッグ開始
     @Override
     public boolean onLongClick(View v) {
         mDragView = v;
@@ -59,13 +65,14 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
         mContainer.removeView(mDragView);
         mContainer.removeView(mInsertPosView);
 
+        // ドラッグしたアイテムの元の位置を覚えておく
         for (int i=0; i<mContainer.getChildCount(); i++) {
             if (mContainer.getChildAt(i) == mDragView) {
-                startPos = i;
+                startPos = insertPos = i;
+                mContainer.addView(mInsertPosView, insertPos);
                 break;
             }
         }
-
         return true;
     }
 
@@ -75,21 +82,18 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
         switch(event.getAction()) {
             case DragEvent.ACTION_DROP: // ドロップ時に呼び出し
             {
-                Log.v("myLog", "" + event.getY());
-                int pos = getDropPosition(event.getX(), event.getY() - 72);
+                int pos = getDropPosition(event.getX(), event.getY());
                 mContainer.removeView(mDragView);
                 mContainer.addView(mDragView, pos);
                 mContainer.removeView(mInsertPosView);
             }
                 break;
-            case DragEvent.ACTION_DRAG_STARTED: { //ドラッグ開始時に呼び出し
-                Log.i("DragSample", "Drag started, event=" + event);
-
-                result = true;
-            } break;
 
             case DragEvent.ACTION_DRAG_ENDED: { //ドラッグ終了時に呼び出し
                 Log.i("DragSample", "Drag ended.");
+
+                // ドロップ位置がコンテナから外れていた場合ACTION_DROPイベントが発生しない。
+                // その場合にドロップ開始時にコンテナから消したViewが消えたままになるので元に戻す
                 boolean foundFlag = false;
                 for (int i=0; i<mContainer.getChildCount(); i++) {
                     if (mContainer.getChildAt(i) == mDragView) {
@@ -104,14 +108,20 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
             } break;
 
             case DragEvent.ACTION_DRAG_LOCATION: { //ドラッグ中に呼び出し
-
-                int pos = getDropPosition(event.getX(), event.getY() - 72);
-                Log.i("DragSample", ""+ event.getY() + " " + pos);
+                // 挿入予定位置に空白のViewを挿入
+                int pos = getDropPosition(event.getX(), event.getY());
+                Log.v("myLog", "insert_pos:" + pos);
                 if (insertPos != pos) {
                     insertPos = pos;
                     mContainer.removeView(mInsertPosView);
                     mContainer.addView(mInsertPosView, pos);
                 }
+                result = true;
+            } break;
+
+            case DragEvent.ACTION_DRAG_STARTED: { //ドラッグ開始時に呼び出し
+                Log.i("DragSample", "Drag started, event=" + event);
+
                 result = true;
             } break;
 
@@ -126,7 +136,6 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
             } break;
 
             default:
-
                 Log.i("DragSample", "other drag event: " + event);
                 result = true;
                 break;
@@ -137,6 +146,7 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
 
     private int getDropPosition(float x, float y) {
         int pos = 0;
+        y -= 72;
         for (int i=0; i<mContainer.getChildCount(); i++) {
             if (mContainer.getChildAt(i) instanceof ImageView) {
                 View v = mContainer.getChildAt(i);
