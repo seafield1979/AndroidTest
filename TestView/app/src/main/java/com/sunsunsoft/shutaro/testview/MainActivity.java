c0a68adb7c9c3627de77e3642f84b5f922973c9d
shusuke unno <seafield1979@gmail.com>
2016-10-21 15:11:35 +0900

    Viewテスト用プロジェクト追加
    TestViewプロジェクト

---
+++ TestView/app/src/main/java/com/sunsunsoft/shutaro/testview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sunsunsoft.shutaro.testview;
+
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setViews();
+    }
+
+    private void setViews() {
+        // toolbar
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+
+        // view pager
+        FragmentManager manager = getSupportFragmentManager();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(manager);
+        viewPager.setAdapter(adapter);
+
+        // tab に view pager のページのタイトルを表示
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+}
